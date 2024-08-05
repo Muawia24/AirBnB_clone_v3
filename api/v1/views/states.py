51b342b7855e04af92442f3538c106da91f80326
Ahmed Muawia <amuawia666@gmail.com>
2024-08-05 23:10:09 +0300

    Sent correct status code

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -39,7 +39,7 @@ def delete_state(state_id):
     return make_response(jsonify({}), 200)
 
 
-@app_views.route('/states/', methods=['POST'], strict_slashes=False)
+@app_views.route('/states', methods=['POST'], strict_slashes=False)
 def post_state():
     """Creates a State """
     json_data = request.get_json()
