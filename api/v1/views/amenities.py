cfa39da3d5e597d05f3cd6b671457e2772dabbfe
Ahmed Muawia <amuawia666@gmail.com>
2024-08-06 13:02:10 +0300

    imported some modules

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -4,8 +4,8 @@ view for Amenity objects that handles all default RESTFul API actions
 """
 from models import storage
 from models.state import State
-from models.state import City
-from models.state import Amenity
+from models.city import City
+from models.amenity import Amenity
 from api.v1.views import app_views
 from flask import jsonify, abort, request, make_response
 
